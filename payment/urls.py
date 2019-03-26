1f80c51cbcdc39f5cac190a5e456a014a2d52fc3
Saruabh Kumar <saukum1999@gmail.com>
2019-03-26 10:34:47 +0530

    Complete Project

---
+++ payment/urls.py
@@ -0,0 +1,12 @@
+# from django.conf.urls import url
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('process/', views.payment_process, name='process'),
+    path('done/', views.payment_done, name='done'),
+    path('canceled/', views.payment_canceled, name='canceled'),
+]
+
+app_name = 'payment'
